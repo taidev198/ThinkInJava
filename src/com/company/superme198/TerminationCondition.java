5ae5fd538897c505ec6c5368fb409423d103c8b9
taidev198 <traigiuxom9x@gmail.com>
2018-10-17 07:51:57 +0700

    Finalize function

--- src/com/company/superme198/TerminationCondition.java
+++ src/com/company/superme198/TerminationCondition.java
@@ -1,4 +1,31 @@
 package com.company.superme198;
 
 public class TerminationCondition {
+
+  static   class Book{
+        boolean checkedOut = false;
+        Book(boolean checkOut){
+            checkedOut = checkOut;
+        }
+
+        void checkIn(){
+            checkedOut = false;
+        }
+
+        protected void finalize(){
+            if (checkedOut){
+                System.out.println("Error: checked out");
+            }
+        }
+    }
+
+    public static void main(String...args){
+        Book book = new Book(true);
+        //proper clean up
+        book.checkIn();
+        //drop reference, forget to clean up
+        new Book(true);
+        //force garbage collection & finalization
+        System.gc();
+    }
 }

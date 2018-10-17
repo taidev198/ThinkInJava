fe1f6beeea6966723721af5bcedf95efb4344a94
taidev198 <traigiuxom9x@gmail.com>
2018-10-17 08:40:52 +0700

    Initial Specify Values

---
+++ src/com/company/superme198/InitialValues2.java
@@ -0,0 +1,14 @@
+package com.company.superme198;
+
+public class InitialValues2 {
+
+    boolean b = false;
+    char ch = 'x';
+    int i = 1;
+    byte aByte = 47;
+    short s = 0xff;
+    long lng = 1;
+    float f = 1.0f;
+    double d = 1.222;
+
+}
